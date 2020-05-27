4d190b295f03be174d87939ea7792dd9d9d65891
JiaFong <jiafongyeh@ieee.org>
2020-05-28 01:33:02 +0800

    first version

---
+++ configs.py
@@ -0,0 +1,8 @@
+save_dir                    = './logs'
+
+miniImageNet_path           	= './filelists/miniImagenet/data'
+miniImageNet_val_path           = './filelists/miniImagenet/val'
+miniImageNet_test_path           = './filelists/miniImagenet/test'
+
+ISIC_path = "./filelists/ISIC"
+EuroSAT_path = "./filelists/EuroSAT/2750"
