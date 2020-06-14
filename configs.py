f7ed27b53a6a614d9500cea5f7f84d26f5f35119
shiannn <b05502087@ntu.edu.tw>
2020-06-14 16:41:26 +0800

    add generation loader to miniimagenet.py and train.py

--- configs.py
+++ configs.py
@@ -1,8 +1,8 @@
 save_dir                    = './logs'
 
-miniImageNet_path           	= './filelists/miniImagenet/data'
-miniImageNet_val_path           = './filelists/miniImagenet/val'
-miniImageNet_test_path           = './filelists/miniImagenet/test'
+miniImageNet_path           	= '/tmp2/rsh/AMMAI_data/miniImagenet/data'
+miniImageNet_val_path           = '/tmp2/rsh/AMMAI_data/miniImagenet/val'
+miniImageNet_test_path           = '/tmp2/rsh/AMMAI_data/miniImagenet/test'
 
-ISIC_path = "./filelists/ISIC"
-EuroSAT_path = "./filelists/EuroSAT/2750"
+ISIC_path = "/tmp2/rsh/AMMAI_data/ISIC"
+EuroSAT_path = "/tmp2/rsh/AMMAI_data/EuroSAT/2750"
